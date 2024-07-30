dde8265c8594baefd3c254705fbf7ca0bead0aef
Emily Kanarek <emilyakanarek@gmail.com>
2024-07-30 11:46:22 -0400

    Set up environment variables

--- server/app.js
+++ server/app.js
@@ -1,9 +1,22 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express from 'express';
 import morgan from 'morgan';
 import ViteExpress from 'vite-express';
+import pg from 'pg';
 
+const { Client } = pg;
 const app = express();
-const port = '8000';
+const port = process.env.PORT || 8000;
+
+const client = new Client({
+    connectionString: process.env.DATABASE_URL
+});
+
+client.connect()
+    .then(() => console.log('Connected to database'))
+    .catch(err => console.error('Connection error', err.stack));
 
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
