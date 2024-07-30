70b6d4bc2f87c2a854dd5fb5ff7942c3ad46fa4b
Emily Kanarek <emilyakanarek@gmail.com>
2024-07-30 13:57:12 -0400

    Debug API endpoint

--- server/app.js
+++ server/app.js
@@ -1,36 +1,39 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-import express from 'express';
-import morgan from 'morgan';
-import ViteExpress from 'vite-express';
-import pg from 'pg';
-import { getRandomPhoto } from './unsplashService.js';
+import express from "express";
+import morgan from "morgan";
+import ViteExpress from "vite-express";
+import pg from "pg";
+import { getRandomPhoto } from "./unsplashService.js";
 
 const { Client } = pg;
 const app = express();
 const port = process.env.PORT || 8000;
 
 const client = new Client({
-    connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
 });
 
-client.connect()
-    .then(() => console.log('Connected to database'))
-    .catch(err => console.error('Connection error', err.stack));
+client
+  .connect()
+  .then(() => console.log("Connected to database"))
+  .catch((err) => console.error("Connection error", err.stack));
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
 
 ViteExpress.config({ printViteDevServerHost: true });
 
-app.get('/api/photo', async (req, res) => {
-    try {
-        const photo = await getRandomPhoto();
-        res.json(photo);
-    } catch (error) {
-        res.status(500).json({ error: 'Failed to fetch photo' });
-    }
+app.get("/api/photo", async (req, res) => {
+  try {
+    const photo = await getRandomPhoto();
+    res.json(photo);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch photo" });
+  }
 });
 
-ViteExpress.listen(app, port, () => console.log(`Server is listening on http://localhost:${port}`));
+ViteExpress.listen(app, port, () =>
+  console.log(`Server is listening on http://localhost:${port}`)
+);
--- server/unsplashService.js
+++ server/unsplashService.js
@@ -1,20 +1,23 @@
-import axios from 'axios';
+import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const UNSPLASH_ACCESS_KEY = process.env.UNSPLASH_ACCESS_KEY;
 
 const unsplashApi = axios.create({
-    baseURL: 'http://api.unsplash.com',
-    headers: {
-        Authorization: `Client-ID ${UNSPLASH_ACCESS_KEY}`
-    }
+  baseURL: "https://api.unsplash.com",
+  headers: {
+    Authorization: `Client-ID ${UNSPLASH_ACCESS_KEY}`,
+  },
 });
 
 export const getRandomPhoto = async () => {
-    try {
-        const response = await unsplashApi.get('/photos/random');
-        return response.data;
-    } catch (error) {
-        console.error('Error fetching photo from Unsplash: ', error);
-        throw error;
-    }
-}
+  try {
+    const response = await unsplashApi.get("/photos/random");
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching photo from Unsplash: ", error);
+    throw error;
+  }
+};
