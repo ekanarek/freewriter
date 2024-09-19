02cdacbf16c19daaa1c27625248b2e0c3de3fd60
Emily Kanarek <emilyakanarek@gmail.com>
2024-09-19 19:58:29 -0400

    Installed Chakra UI

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { AuthProvider } from "./contexts/AuthContext.jsx";
+import { ChakraProvider } from '@chakra-ui/react';
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
+      <ChakraProvider>
       <App />
+      </ChakraProvider>
     </AuthProvider>
   </React.StrictMode>
 );
